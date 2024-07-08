a5615ebaaabb9f965fdf556e060522da60a805e6
Rafay Yousafzai <rafaykhan.mesh@gmail.com>
2024-07-09 02:59:32 +0500

    auth

---
+++ app/(auth)/sign-up.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const SignUp = () => {
+  return (
+    <View>
+      <Text>SignIn</Text>
+    </View>
+  );
+};
+
+export default SignUp;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -41,8 +41,8 @@ const RootLayout = () => {
     // <GlobalProvider>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
-        {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="search/[query]" options={{ headerShown: false }} /> */}
       </Stack>
     // </GlobalProvider>
