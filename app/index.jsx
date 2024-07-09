2c4603f3f5df779e2253e0467b51435ff2bf0b0c
Rafay Yousafzai <rafaykhan.mesh@gmail.com>
2024-07-09 08:54:12 +0500

    tabs screen

--- app/index.jsx
+++ app/index.jsx
@@ -3,9 +3,9 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center align-middle">
-      <Text className="font-bold text-3xl font-pblack">Aora</Text>
-      <Link href={"home"} >dsa</Link>
+    <View className="flex-1 items-center justify-center ">
+      <Text className=" text-3xl font-pblack">Aora</Text>
+      <Link href={"home"}>Go</Link>
     </View>
   );
 }
